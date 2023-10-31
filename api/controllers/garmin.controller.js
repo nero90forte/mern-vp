bc9af6e8ff623892ef8dc71744dc0d9f92f3a21b
nero90forte <neroforte90@gmail.com>
2023-10-31 08:46:20 +0700

    Fix controller

--- api/controllers/garmin.controller.js
+++ api/controllers/garmin.controller.js
@@ -27,44 +27,44 @@
   
 //   const https = require('https')
 
-export function post(url, data) {
-  const dataString = JSON.stringify(data)
+// export function post(url, data) {
+//   const dataString = JSON.stringify(data)
 
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': dataString.length,
-    },
-    timeout: 1000, // in ms
-  }
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Content-Length': dataString.length,
+//     },
+//     timeout: 1000, // in ms
+//   }
 
-  return new Promise((resolve, reject) => {
-    const req = https.request(url, options, (res) => {
-      if (res.statusCode < 200 || res.statusCode > 299) {
-        return reject(new Error(`HTTP status code ${res.statusCode}`))
-      }
+//   return new Promise((resolve, reject) => {
+//     const req = https.request(url, options, (res) => {
+//       if (res.statusCode < 200 || res.statusCode > 299) {
+//         return reject(new Error(`HTTP status code ${res.statusCode}`))
+//       }
 
-      const body = []
-      res.on('data', (chunk) => body.push(chunk))
-      res.on('end', () => {
-        const resString = Buffer.concat(body).toString()
-        resolve(resString)
-      })
-    })
+//       const body = []
+//       res.on('data', (chunk) => body.push(chunk))
+//       res.on('end', () => {
+//         const resString = Buffer.concat(body).toString()
+//         resolve(resString)
+//       })
+//     })
 
-    req.on('error', (err) => {
-      reject(err)
-    })
+//     req.on('error', (err) => {
+//       reject(err)
+//     })
 
-    req.on('timeout', () => {
-      req.destroy()
-      reject(new Error('Request time out'))
-    })
+//     req.on('timeout', () => {
+//       req.destroy()
+//       reject(new Error('Request time out'))
+//     })
 
-    req.write(dataString)
-    req.end()
-  })
-}
+//     req.write(dataString)
+//     req.end()
+//   })
+// }
     
-const res = await post('https://...', data)
+// const res = await post('https://...', data)
--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import garminRouter from './routes/garmin.route.js';
+// import garminRouter from './routes/garmin.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
